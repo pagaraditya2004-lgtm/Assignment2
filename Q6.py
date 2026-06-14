19bee7191c616ff0ce9ec0274d83288c91dca3e3
Aditya Pagar <pagaraditya2004@gmail.com>
2026-06-15 00:52:24 +0530

    Initial commit

---
+++ Q6.py
@@ -0,0 +1,10 @@
+print("Enter no1 : ")
+no1 = float(input())
+
+print("Enter no2 : ")
+no2 = float(input())
+
+print("Addition : ", no1+no2)
+print("Subtraction : ", no1-no2)
+print("Multiplication : ", no1*no2)
+print("Division : ", no1/no2)
